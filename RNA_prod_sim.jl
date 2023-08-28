dba90ce9de97876b7b15ecc03b43dec03e3a8d5e
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-08-27 21:18:21 -0400

    added discrete events to RNA prod simulation

--- RNA_prod_sim.jl
+++ RNA_prod_sim.jl
@@ -1,4 +1,5 @@
-using Catalyst, DifferentialEquations, Plots
+using Catalyst, DifferentialEquations, Plots, Interpolations
+using Statistics, Distributions
 
 rn = @reaction_network begin
     (kOn,kOff), A + DNA <--> A_DNA
@@ -6,11 +7,11 @@ rn = @reaction_network begin
     kT, RNA --> RNA + GFP
     deg_R, RNA --> 0
     deg_G, GFP --> 0
-end kOn kOff k kT deg_R deg_G
+end
 
 tspan = (0.0, 300.0)
 u0 = [:A => 1, :DNA => 1, :A_DNA => 0, :RNA => 0, :GFP => 0]
-p = [:kOn => 0.0042*60*60, :kOff => 0.0038*60*60, :k => 250, :kT => 10.0, :deg_R => 0.8, :deg_G => 0.3]
+p = [:kOn => 0.0042*60*60, :kOff => 0.00038*60*60, :k => 250, :kT => 10.0, :deg_R => 0.8, :deg_G => 0.3]
 
 dprob = DiscreteProblem(rn, u0, tspan, p)
 jprob = JumpProblem(rn, dprob, Direct())
@@ -26,7 +27,17 @@ plot(sol,idxs=5)
 t_inter = 0:0.001:300
 gfp_int = linear_interpolation(sol.t, sol[5,:])
 gfp = gfp_int.(t_inter)
+rna_int = linear_interpolation(sol.t, sol[4,:])
+rna = rna_int.(t_inter)
 
-histogram(gfp[50000:end])
+plot(rna)
+
+rna_mean = mean(rna[25000:end])
+rna_std = std(rna[25000:end])
+rna_norm = Normal(rna_mean,rna_std)
+
+histogram(rna[25000:end], normed=true)
+plot!(80:0.1:350,pdf.(rna_norm,80:0.1:350))
+
 plot(t_inter,gfp,xlim=(100,110))
 
---
+++ RNA_prod_sim_events.jl
@@ -0,0 +1,28 @@
+using Catalyst, DifferentialEquations, Plots, Interpolations
+using Statistics, Distributions
+
+@parameters kOn kOff k kT deg_R deg_G
+@variables t
+@species A(t) DNA(t) A_DNA(t) RNA(t) GFP(t)
+
+rxs = [(@reaction kOn, A + DNA --> A_DNA),
+       (@reaction kOff, A_DNA --> A + DNA),
+       (@reaction k, A_DNA --> A_DNA + RNA),
+       (@reaction kT, RNA --> RNA + GFP),
+       (@reaction deg_R, RNA --> 0),
+       (@reaction deg_G, GFP --> 0)]
+
+discrete_events = (t == 100.0) => [k ~ 500]
+
+tspan = (0.0, 300.0)
+u0 = [A => 1, DNA => 1, A_DNA => 0, RNA => 0, GFP => 0]
+p = [kOn => 0.0042*60*60, kOff => 0.00038*60*60, k => 250, kT => 10.0, deg_R => 0.8, deg_G => 0.3]
+
+@named rn = ReactionSystem(rxs, t, [A, DNA, A_DNA, RNA, GFP], [kOn, kOff, k, kT, deg_R, deg_G]; discrete_events)
+
+dprob = DiscreteProblem(rn, u0, tspan, p)
+jprob = JumpProblem(rn, dprob, Direct())
+
+sol = solve(jprob, SSAStepper(); tstops = [100.0])
+
+plot(sol)
